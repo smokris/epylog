7404301c271c5a2f35e95bb23a9d7d3206bd2573
Konstantin Ryabitsev <icon@mricon.com>
2003-04-18 04:12:11 +0000

    it's 2003.

--- py/epylog/__init__.py
+++ py/epylog/__init__.py
@@ -2,7 +2,7 @@
 Description will eventually go here.
 """
 ##
-# Copyright (C) 2002 by Duke University
+# Copyright (C) 2003 by Duke University
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
